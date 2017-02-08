ddeb1b0c35e7b2ac157ef9bfa1a091c86b7ebeca
karachev <pavelkarachev@ymail.com>
2017-02-08 20:41:41 +0300

    0.6.2.1
    Добавил модальное окно для карты.

--- js/style.js
+++ js/style.js
@@ -2,6 +2,10 @@ var link = document.querySelector(".button-write-us");
 var popup = document.querySelector(".write-us");
 var close = document.querySelector(".write-us-close");
 
+var map = document.querySelector(".map-button");
+var popupMap = document.querySelector(".show-map");
+var closeMap = document.querySelector(".map-close");
+
 link.addEventListener("click", function(event){
 	event.preventDefault();
 	popup.classList.add("write-us-show");
@@ -12,6 +16,12 @@ close.addEventListener("click", function(event){
 	popup.classList.remove("write-us-show");
 });
 
+map.addEventListener("click", function(event){
+	event.preventDefault();
+	popupMap.classList.add("show-map-open");
+});
 
-
-
+closeMap.addEventListener("click", function(event){
+	event.preventDefault();
+	popupMap.classList.remove("show-map-open");
+});

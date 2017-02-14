a1caebbf7485c254445ed88a1645b5fb6f757e24
karachev <pavelkarachev@ymail.com>
2017-02-14 14:39:24 +0300

    1.0.6
    Табуляция Less и JS

--- js/style.js
+++ js/style.js
@@ -7,21 +7,21 @@ var popupMap = document.querySelector(".show-map");
 var closeMap = document.querySelector(".map-close");
 
 link.addEventListener("click", function(event){
-	event.preventDefault();
-	popup.classList.add("write-us-show");
+  event.preventDefault();
+  popup.classList.add("write-us-show");
 });
 
 close.addEventListener("click", function(event){
-	event.preventDefault();
-	popup.classList.remove("write-us-show");
+  event.preventDefault();
+  popup.classList.remove("write-us-show");
 });
 
 map.addEventListener("click", function(event){
-	event.preventDefault();
-	popupMap.classList.add("show-map-open");
+  event.preventDefault();
+  popupMap.classList.add("show-map-open");
 });
 
 closeMap.addEventListener("click", function(event){
-	event.preventDefault();
-	popupMap.classList.remove("show-map-open");
+  event.preventDefault();
+  popupMap.classList.remove("show-map-open");
 });
